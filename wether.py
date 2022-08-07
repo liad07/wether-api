7d2971d6a316b412474b53540244ecb084bc589f
liadk07 <91607695+liad07@users.noreply.github.com>
2022-08-07 22:10:19 +0300

    Add files via upload

---
+++ wether.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import *
+import json
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index(city="not insert city"):
+    city = str(request.args.get('city'))
+    url=f"https://www.google.com/search?q=wether in {city}"
+    r=requests.get(url)
+    s=BeautifulSoup(r.text,"html.parser")
+    wether=s.find("div",class_="BNeawe").text
+    info=s.find("div",class_="BNeawe tAd8D AP7Wnd").text
+    json_dump={"city":city,"wether":wether,"info":info.split("\n")[1]}
+    print(json_dump)
+    return json_dump
+app.run(host='0.0.0.0', port=80)
